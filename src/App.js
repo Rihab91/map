b5f6c20bed036b1c467a7bfebd29e50f74fa9e68
rihab91 <rihab.rjab@hotmail.com>
2024-02-15 17:18:33 +0100

    map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from 'styled-components'
+import Map from "./components/Map";
 
+
+const AppContainer =styled.div`
+diplay :flex;
+justify-content:center;
+align-items:center;
+height:100vh;
+`;
+const mapContainerStyle={
+  width:'600px',
+  height:'400px',
+};
+
+const center={
+  lat: 35.8245,
+  lng: 10.6346,
+};
+
+const zoom = 13;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <AppContainer>
+    <Map 
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={zoom}/>
+    </AppContainer>
+  )
 }
 
 export default App;
