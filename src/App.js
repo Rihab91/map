a68abea0902fbe24f6a53818ed416b3cf65e386b
rihab91 <rihab.rjab@hotmail.com>
2024-02-16 13:11:49 +0100

    map2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from "react";
+// Import styled-components for styling
 import styled from 'styled-components'
+// Import styled-components for styling
 import Map from "./components/Map";
 
-
+// Styled component for the main container of the application
 const AppContainer =styled.div`
 diplay :flex;
+height:100vh;
 justify-content:center;
 align-items:center;
-height:100vh;
-`;
-const mapContainerStyle={
-  width:'600px',
-  height:'400px',
-};
 
+`;
+// Style configuration for the map container
+const mapContainerStyle={
+  width:'700px',
+  height:'500px',
+};
+// Center coordinates for the map
 const center={
   lat: 35.8245,
   lng: 10.6346,
 };
 
 const zoom = 13;
+// App component, the main component of the application
 function App() {
   return(
     <AppContainer>
     <Map 
+     // Pass the map container style as prop
         mapContainerStyle={mapContainerStyle}
         center={center}
         zoom={zoom}/>
--- src/components/Map.js
+++ src/components/Map.js
@@ -3,16 +3,22 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Map = ({mapContainerStyle, center ,zoom}) =>{
     return(
+        
         <>
-
+{/*  LoadScript component ensures that Google Maps JavaScript API is loaded */}
 <LoadScript
         googleMapsApiKey="AIzaSyBB9B-Rk8go54u0Ty2z-gNS28IhIQbG_lg">
+            {/* GoogleMap component renders the map */}
         <GoogleMap
+         // Styling for the map container
         mapContainerStyle={mapContainerStyle}
+        // The geographical center of the map
         center={center}
+        // The zoom level of the map
         zoom={zoom} 
         >
         <Marker position={center} />
+        {/* Marker component renders a marker at the specified center position */}
         </GoogleMap>
         </LoadScript>
         </>
